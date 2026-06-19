8f6a1c7fb591e52d4defc6a24fa93da2f9e4c055
Loy Yee Ko <koloyyee@gmail.com>
2026-06-19 13:47:46 -0400

    async connect to docker pg

--- app/db.py
+++ app/db.py
@@ -1,9 +1,17 @@
+import asyncio
+from collections.abc import AsyncGenerator
 import os 
 
 from pathlib import Path
 from typing import Annotated
 from fastapi import Depends
-from sqlmodel import SQLModel, create_engine, Session
+#from sqlmodel import SQLModel, create_engine, Session
+from sqlalchemy.orm import Session, DeclarativeBase
+from sqlalchemy import text 
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+class Base(DeclarativeBase):
+	pass
 
 def get_db_url():
 	secret_path = Path("/run/secrets/db-password")
@@ -12,19 +20,30 @@ def get_db_url():
 	else:
 		password = os.getenv("DB_PASSWORD", default="password")
 	
-	return f"postgresql+asyncpg://appuser:{password}@db:5432/sec_daily"
+	host = os.getenv("DB_HOST", default="localhost")
+	return f"postgresql+asyncpg://appuser:{password}@{host}:5432/insider-daily"
 
-engine = create_engine(get_db_url())
+engine = create_async_engine(get_db_url(), echo=True)
+AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=True)
+async def get_async_session() -> AsyncGenerator[AsyncSession]:
+	async with AsyncSessionLocal() as session:
+		yield session
 
-def create_db_and_tables():
-	SQLModel.metadata.create_all(engine)
+# Smoke test
+if __name__ == "__main__":
+	import asyncio
+	async def _test():
+		async with AsyncSessionLocal() as session:
+			result = await session.execute(text("select 'hello world'"))
+			print(result.fetchall())
 
-def get_session():
-    with Session(engine) as session:
-        yield session
+	asyncio.run(_test())
+
+#def create_db_and_tables():
+#	Base.metadata.create_all(engine)
 
 
-SessionDep = Annotated[Session, Depends(get_session)]
+#SessionDep = Annotated[Session, Depends(get_session)]
 
 	
 	
